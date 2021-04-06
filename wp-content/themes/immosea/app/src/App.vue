fe08a6de5e3e546c3e903d8f4f677e6ca0b5d843
Jeka Savchyna <jeka.tibor@gmail.com>
2021-04-06 15:02:03 +0300

    Small refactoring navigation

--- wp-content/themes/immosea/app/src/App.vue
+++ wp-content/themes/immosea/app/src/App.vue
@@ -10,8 +10,12 @@ import Vue from 'vue'
 import Form from './components/Form'
 import store from './store'
 import Vuelidate from 'vuelidate'
+import VueCookies from 'vue-cookies'
+
 
 Vue.use(Vuelidate)
+Vue.use(VueCookies)
+
 
 export default {
   name: 'App',
--- wp-content/themes/immosea/app/src/components/Form.vue
+++ wp-content/themes/immosea/app/src/components/Form.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="form">
         <div class="form__holder">
-            <div v-for="(item, index) in questions"
+            <div v-for="(item, index) in filterSteps"
                  :key="index"
                  v-show="activeStep === index"
             >
                 <component
-                        :is="item.component"
-                        :title="item.title"
-                        :text="item.text"
-                        :showPrice="item.showPrice"
+                        :is="questions[item.step].component"
+                        :title="questions[item.step].title"
+                        :text="questions[item.step].text"
+                        :showPrice="questions[item.step].showPrice"
                         :buttonPrev="{
                             title: buttonPrev.title,
                             click: showPrevScreen,
@@ -46,6 +46,8 @@
   import Step14 from './Steps/Step14'
   import Step15 from './Steps/Step15'
   import { questions } from '../Data/questions'
+
+
 
   export default {
     name: 'Form',
@@ -140,6 +142,10 @@
             condition: true
           }
         ]
+      },
+      filterSteps() {
+        console.log(this.condition.filter(i => i.condition));
+        return this.condition.filter(i => i.condition)
       }
     },
     created() {
@@ -150,29 +156,39 @@
         this.$store.dispatch('fetchProducts')
       },
       showNextScreen() {
-        this.buttonPrev.show = true
-
-        if (this.activeStep < this.questions.length - 1) {
-          this.passedSteps.push(this.activeStep)
-          this.activeStep = this.findNextScreen(this.activeStep)
+        if (this.activeStep < this.filterSteps.length - 1) {
+          this.activeStep++
         }
+
+        // if (this.activeStep < this.questions.length - 1) {
+        //   this.passedSteps.push(this.activeStep)
+        //   this.activeStep = this.findNextScreen(this.activeStep)
+        //
+        //   // this.$cookies.set('passedSteps', this.passedSteps)
+        //   // this.$cookies.set('activeStep', this.activeStep)
+        // }
 
         window.scrollTo(0,0);
       },
       showPrevScreen() {
-        this.activeStep = this.passedSteps[this.passedSteps.length - 1]
+        // this.activeStep = this.passedSteps[this.passedSteps.length - 1]
         this.buttonPrev.show = true
-        this.buttonNext.title = 'Weiter'
-        this.passedSteps.pop(this.passedSteps.length - 1)
+        // this.buttonNext.title = 'Weiter'
+        // this.passedSteps.pop(this.passedSteps.length - 1)
 
-        if (this.activeStep === 0) {
+        // this.$cookies.set('passedSteps', JSON.stringify(this.passedSteps))
+        // this.$cookies.set('activeStep', this.activeStep)
+
+        if (this.activeStep !== 0) {
+          this.activeStep--
+        } else {
           this.buttonPrev.show = false
         }
         window.scrollTo(0,0);
       },
-      findNextScreen(index) {
-        return this.condition.find(i => i.step > index && i.condition).step
-      }
+      // findNextScreen(index) {
+      //   return this.condition.find(i => i.step > index && i.condition).step
+      // }
     }
   }
 </script>
