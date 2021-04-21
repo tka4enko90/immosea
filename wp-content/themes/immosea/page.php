05b8acc36e76ee77c01fb1fd0387ab490fc034d1
Alexander Tkachenko <sasha@markupus.tech>
2021-04-21 14:42:53 +0300

    updated checkout thankyou page

--- wp-content/themes/immosea/page.php
+++ wp-content/themes/immosea/page.php
@@ -1,16 +1,14 @@
-<?php
-use Timber\Timber;
-if (class_exists('Timber')) {
-    $context['options'] = Timber::get_context();
-    if (class_exists('CustomMultipagePlugin')) {
-        $CustomMultipagePlugin = new CustomMultipagePlugin($context);
-    }
-
-    get_header();
-    if (class_exists('Timber')) {
-        if ($CustomMultipagePlugin) {
-            $CustomMultipagePlugin->renderPage($context);
-        }
-    }
-}
-get_footer();
+<?php get_header();
+the_post();
+?>
+    <main class="main">
+        <div class="container">
+            <div class="post-single">
+                <div class="post-single-content">
+                    <h2 class="text-primary"><?php echo get_the_title(); ?></h2>
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php get_footer(); ?>
